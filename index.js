7ec9998909f371e815cd259fdea626736e14564a
Marwa Hasan <111687567+MarwaHasan54@users.noreply.github.com>
2022-11-07 14:30:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImg1 = "dice" + randomNumber1 + ".png";
+
+var randomImgSource1 = "images/" + randomImg1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImgSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImg2 = "dice" + randomNumber2 + ".png";
+
+var randomImgSource2 = "images/" + randomImg2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImgSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
